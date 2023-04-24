797ea815fd16ccf8d78b95e10947e0e3a6e799c6
CJ-de-V <CorneldeVilliers01@gmail.com>
2023-04-24 19:44:58 +0200

    first small dataset, pre data extraction

--- fullSetSimulation.sh
+++ fullSetSimulation.sh
@@ -2,7 +2,7 @@ for i in 32 64 128 256 512
 do
 	mkdir "N$i"
 	cd "N$i"
-	../a.out $i straight.config
-	mpirun -np 4 python3 simple.py ../in.setup
+	../a.out $i config.straight
+	mpirun -np 4 python3 ../batchrun.py ../in.setup
 	cd ..
 done
